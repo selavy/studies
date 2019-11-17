1820be133add275591f9cc286fc22793da19ca4a
Peter Lesslie <pclesslie@gmail.com>
2019-11-17 15:13:37 -0600

    Switching to Catch

--- integer/intalu.c
+++ integer/intalu.c
@@ -2,5 +2,17 @@
 
 int8_t intalu_adds8(int8_t x, int8_t y)
 {
-    return 0;
+    uint8_t result, a, b, r, c, i;
+
+    result = 0;
+    c = 0;
+    for (i = 0; i < 8; ++i) {
+        a = x & (1u << i) ? 1 : 0;
+        b = y & (1u << i) ? 1 : 0;
+        c = ((a & b) & !c) + ((a | b) & c);
+        r = (!(a ^ b) & c) + ((a ^ b) & !c);
+        result = result | (r << i);
+    }
+
+    return result;
 }
--- integer/intalu_test.c
+++ integer/intalu_test.c
@@ -1,13 +1,21 @@
 #include <cgreen/cgreen.h>
 #include "intalu.h"
 
-Ensure(integer_1_plus_2_equals_3)
+#define ASIZE(x) (sizeof(x) / sizeof(x[0]))
+
+Ensure(integer_8bit_plus_one)
 {
-    assert_that(intalu_adds8(1, 2), is_equal_to(3));
+    int8_t vs[] = { 1, 2, 3, 4, 5, 6, 100, 125 };
+    for (size_t i = 0; i < ASIZE(vs); ++i) {
+        int8_t a = vs[i];
+        int8_t b = 1;
+        int8_t e = a + b;
+        assert_that(intalu_adds8(a, b), is_equal_to(e));
+    }
 }
 
 int main(int argc, char **argv) {
     TestSuite *suite = create_test_suite();
-    add_test(suite, integer_1_plus_2_equals_3);
+    add_test(suite, integer_8bit_plus_one);
     return run_test_suite(suite, create_text_reporter());
 }
