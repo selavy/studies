c9d961e86f0508f80658609bc387a6f1e52b4e05
Peter Lesslie <pclesslie@gmail.com>
2019-11-17 16:04:56 -0600

    More tests

--- integer/intalu.cpp
+++ integer/intalu.cpp
@@ -1,6 +1,6 @@
 #include "intalu.h"
 
-int8_t intalu_adds8(int8_t x, int8_t y)
+uint8_t intalu_addu8(uint8_t x, uint8_t y)
 {
     uint8_t result, a, b, r, c, i;
 
@@ -15,4 +15,14 @@ int8_t intalu_adds8(int8_t x, int8_t y)
     }
 
     return result;
+}
+
+int8_t intalu_adds8(int8_t x, int8_t y)
+{
+    return intalu_addu8(x, y);
+}
+
+int8_t intalu_subs8(int8_t x, int8_t y)
+{
+    return intalu_adds8(x, -y);
 }
--- integer/intalu_test.cpp
+++ integer/intalu_test.cpp
@@ -3,6 +3,7 @@
 #include "intalu.h"
 #include <vector>
 #include <limits>
+#include <random>
 
 
 TEST_CASE("Add positive 8-bit signed integer with 1")
@@ -85,7 +86,7 @@ TEST_CASE("Add positive and negative 8-bit signed integers")
     }
 }
 
-TEST_CASE("Positive 8-bit signed integer underflow -- 2s compliment underflow behavior")
+TEST_CASE("8-bit signed integer underflow -- 2s compliment underflow behavior")
 {
     SECTION("Add -1 to CHAR_MIN")
     {
@@ -112,5 +113,76 @@ TEST_CASE("Positive 8-bit signed integer underflow -- 2s compliment underflow be
         int8_t expect = 0;
         int8_t result = intalu_adds8(a, b);
         REQUIRE(result == expect);
+    }
+}
+
+TEST_CASE("Subtract 8-bit integers")
+{
+    std::vector<std::pair<int8_t, int8_t>> vs = {
+        { 0, -1 },
+        { 125, -125 },
+        { 100, -127 },
+        { 127, -128 },
+        { 5, -128 },
+        { 10, -15 },
+        { 10, -5 },
+        { 10, 10 },
+        { 10, 5 },
+        { 10, 15 },
+        { 15, 25 },
+    };
+
+    for (auto [a, b] : vs) {
+        SECTION("a - b")
+        {
+            int8_t expect = a - b;
+            int8_t result = intalu_subs8(a, b);
+            REQUIRE(result == expect);
+        }
+
+        SECTION("b - a")
+        {
+            int8_t expect = b - a;
+            int8_t result = intalu_subs8(b, a);
+            REQUIRE(result == expect);
+        }
+    }
+}
+
+TEST_CASE("Randomized 8-bit add and sub")
+{
+    for (int i = 0; i < 100; ++i) {
+        int8_t a = rand();
+        int8_t b = rand();
+
+        INFO("a = " << static_cast<int>(a) << ", b = " << static_cast<int>(b));
+
+        SECTION("a + b")
+        {
+            int8_t expect = a + b;
+            int8_t result = intalu_adds8(a, b);
+            REQUIRE(result == expect);
+        }
+
+        SECTION("b + a")
+        {
+            int8_t expect = b + a;
+            int8_t result = intalu_adds8(b, a);
+            REQUIRE(result == expect);
+        }
+
+        SECTION("a - b")
+        {
+            int8_t expect = a - b;
+            int8_t result = intalu_subs8(a, b);
+            REQUIRE(result == expect);
+        }
+
+        SECTION("b - a")
+        {
+            int8_t expect = b - a;
+            int8_t result = intalu_subs8(b, a);
+            REQUIRE(result == expect);
+        }
     }
 }
