4d1cee37335d067fc72e608fdec2c49490ab4e20
Peter Lesslie <pclesslie@gmail.com>
2019-11-19 15:08:34 -0600

    Switch s8 -> i8

--- integer/intalu.cpp
+++ integer/intalu.cpp
@@ -1,20 +1,19 @@
 #include "intalu.h"
 
-uint8_t  intalu_negu8(uint8_t x)
+uint8_t intalu_neg_u8(uint8_t x)
 {
     // return ~x + 1;
-    return intalu_addu8(~x, 1);
+    return intalu_add_u8(~x, 1);
 }
 
-int8_t  intalu_negs8(int8_t x)
+int8_t intalu_neg_i8(int8_t x)
 {
-    return intalu_negu8(x);
+    return intalu_neg_u8(x);
 }
 
-uint8_t intalu_addu8(uint8_t x, uint8_t y)
+uint8_t intalu_add_u8(uint8_t x, uint8_t y)
 {
     uint8_t result, a, b, r, c, i;
-
     result = 0;
     c = 0;
     for (i = 0; i < 8; ++i) {
@@ -24,23 +23,22 @@ uint8_t intalu_addu8(uint8_t x, uint8_t y)
         c = (a & b) + ((a ^ b)*c);
         result = result | (r << i);
     }
-
     return result;
 }
 
-int8_t intalu_adds8(int8_t x, int8_t y)
+int8_t intalu_add_i8(int8_t x, int8_t y)
 {
-    return intalu_addu8(x, y);
+    return intalu_add_u8(x, y);
 }
 
-uint8_t intalu_subu8(uint8_t x, uint8_t y)
+uint8_t intalu_sub_u8(uint8_t x, uint8_t y)
 {
-    return intalu_adds8(x, intalu_negs8(y));
+    return intalu_add_i8(x, intalu_neg_i8(y));
 }
 
-int8_t intalu_subs8(int8_t x, int8_t y)
+int8_t intalu_sub_i8(int8_t x, int8_t y)
 {
-    return intalu_subu8(x, y);
+    return intalu_sub_u8(x, y);
 }
 
 static uint16_t _mulu8_ext_u16(uint16_t x, uint16_t y)
@@ -76,12 +74,12 @@ static uint16_t _mulu8_ext_u16(uint16_t x, uint16_t y)
     return result;
 }
 
-uint8_t intalu_mulu8(uint8_t x, uint8_t y)
+uint8_t intalu_mul_u8(uint8_t x, uint8_t y)
 {
     return _mulu8_ext_u16(x, y);
 }
 
-int8_t intalu_muls8( int8_t x,  int8_t y)
+int8_t intalu_mul_i8( int8_t x,  int8_t y)
 {
     uint16_t x_neg  = x < 0;
     uint16_t y_neg  = y < 0;
@@ -92,7 +90,7 @@ int8_t intalu_muls8( int8_t x,  int8_t y)
     return is_neg ? -result : result;
 }
 
-uint8_t intalu_divu8(uint8_t n, uint8_t d)
+uint8_t intalu_div_u8(uint8_t n, uint8_t d)
 {
     uint8_t quot = 0;
     uint8_t rem  = 0;
@@ -106,28 +104,28 @@ uint8_t intalu_divu8(uint8_t n, uint8_t d)
     return quot;
 }
 
-uint8_t _absu8(int8_t x)
+uint8_t _abs_u8(int8_t x)
 {
     if (x >= 0)
         return x;
     else if (x == -128)
         return 128;
     else
-        return intalu_negs8(x);
+        return intalu_neg_i8(x);
 }
 
-int8_t intalu_divs8(int8_t n, int8_t d)
+int8_t intalu_div_i8(int8_t n, int8_t d)
 {
-    uint8_t quot = intalu_divu8(_absu8(n), _absu8(d));
+    uint8_t quot = intalu_div_u8(_abs_u8(n), _abs_u8(d));
     return ((n < 0) ^ (d < 0)) ? -quot : quot;
 }
 
-uint8_t intalu_fmau8(uint8_t a, uint8_t b, uint8_t c)
+uint8_t intalu_fma_u8(uint8_t a, uint8_t b, uint8_t c)
 {
-    return intalu_addu8(intalu_mulu8(a, b), c);
+    return intalu_add_u8(intalu_mul_u8(a, b), c);
 }
 
-int8_t intalu_fmas8(int8_t a, int8_t b, int8_t c)
+int8_t intalu_fma_i8(int8_t a, int8_t b, int8_t c)
 {
-    return intalu_adds8(intalu_muls8(a, b), c);
+    return intalu_add_i8(intalu_mul_i8(a, b), c);
 }
--- integer/intalu.h
+++ integer/intalu.h
@@ -2,19 +2,15 @@
 
 #include <stdint.h>
 
-uint8_t  intalu_negu8(uint8_t x);
- int8_t  intalu_negs8( int8_t x);
-
-uint8_t intalu_addu8(uint8_t x, uint8_t y);
- int8_t intalu_adds8( int8_t x,  int8_t y);
-uint8_t intalu_subu8(uint8_t x, uint8_t y);
- int8_t intalu_subs8( int8_t x,  int8_t y);
-
-uint8_t intalu_mulu8(uint8_t x, uint8_t y);
- int8_t intalu_muls8( int8_t x,  int8_t y);
-
-uint8_t intalu_divu8(uint8_t n, uint8_t d);
- int8_t intalu_divs8( int8_t n,  int8_t d);
-
-uint8_t intalu_fmau8(uint8_t a, uint8_t b, uint8_t c);
- int8_t intalu_fmas8( int8_t a,  int8_t b,  int8_t c);
+uint8_t intalu_neg_u8(uint8_t x);
+ int8_t intalu_neg_i8( int8_t x);
+uint8_t intalu_add_u8(uint8_t x, uint8_t y);
+ int8_t intalu_add_i8( int8_t x,  int8_t y);
+uint8_t intalu_sub_u8(uint8_t x, uint8_t y);
+ int8_t intalu_sub_i8( int8_t x,  int8_t y);
+uint8_t intalu_mul_u8(uint8_t x, uint8_t y);
+ int8_t intalu_mul_i8( int8_t x,  int8_t y);
+uint8_t intalu_div_u8(uint8_t n, uint8_t d);
+ int8_t intalu_div_i8( int8_t n,  int8_t d);
+uint8_t intalu_fma_u8(uint8_t a, uint8_t b, uint8_t c);
+ int8_t intalu_fma_i8( int8_t a,  int8_t b,  int8_t c);
