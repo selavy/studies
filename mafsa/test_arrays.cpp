9ba112da375292fc2ca420c130f08d7b1d5b9358
Peter Lesslie <pclesslie@gmail.com>
2020-04-17 12:36:17 -0500

    Setup test

--- mafsa/test_arrays.cpp
+++ mafsa/test_arrays.cpp
@@ -1,7 +1,117 @@
 #include <catch2/catch.hpp>
 #include "darray.h"
 
+// clang-format off
+const std::vector<std::string> DICT = {
+    "AA",
+    "AAH",
+    "AAHED",
+    "AAHING",
+    "AAHS",
+    "AAL",
+    "AALII",
+    "AALIIS",
+    "AALS",
+    "AARDVARK",
+    "AARDVARKS",
+    "AARDWOLF",
+    "AARDWOLVES",
+    "CEANOTHUS",
+    "CEANOTHUSES",
+    "CEAS",
+    "CEASE",
+    "CEASED",
+    "CEASEFIRE",
+    "CEASEFIRES",
+    "CEASELESS",
+    "MIDWIVING",
+    "MIDYEAR",
+    "MIDYEARS",
+    "MIELIE",
+    "MIELIES",
+    "GAUJES",
+    "GAULEITER",
+    "GAULEITERS",
+    "THEATERLANDS",
+    "THEATERS",
+    "THEATRAL",
+    "TYPEY",
+    "TYPHACEOUS",
+    "TYPHLITIC",
+    "TYPHLITIS",
+    "UPDATING",
+    "UPDIVE",
+    "UPDIVED",
+    "VICTIMIZE",
+    "VOICER",
+    "VOICERS",
+    "VOICES",
+    "VOICING",
+    "WILDCATTINGS",
+    "WILDEBEEST",
+    "WILDEBEESTS",
+    "WILDED",
+    "WOOFTAH",
+    "WOOFTAHS",
+    "WOOFTER",
+    "YAMPY",
+    "YAMS",
+    "YAMULKA",
+    "ZYMOSANS",
+    "ZYMOSES",
+    "ZYMOSIMETER",
+    "ZYMOSIMETERS",
+    "ZYMOSIS",
+};
+// clang-format on
+
+// clang-format off
+const std::vector<std::string> MISSING = {
+    "ABIDDEN",
+    "ABIDE",
+    "ABIDED",
+    "ABIDER",
+    "ACCOUSTREMENTS",
+    "ACCOUTER",
+    "ACCOUTERED",
+    "FUTURISTIC",
+    "FUTURISTICALLY",
+    "FUTURISTICS",
+    "XIPHOIDAL",
+    "XIPHOIDS",
+    "XIPHOPAGI",
+    "ZZZ",
+    "ZAZAZ",
+    "AAAA",
+    "AZZZZ",
+    "YZY",
+};
+// clang-format on
+
 TEST_CASE("Darray")
 {
-    REQUIRE(1 == 2);
+    Darray d;
+    for (const auto& word : DICT) {
+        d.insert(word);
+    }
+
+    for (const auto& word : DICT) {
+        CHECK(d.isword(word) == true);
+    }
+
+    for (const auto& word : MISSING) {
+        CHECK(d.isword(word) == false);
+    }
+
+    for (const auto& word : DICT) {
+        d.insert(word);
+    }
+
+    for (const auto& word : DICT) {
+        CHECK(d.isword(word) == true);
+    }
+
+    for (const auto& word : MISSING) {
+        CHECK(d.isword(word) == false);
+    }
 }
