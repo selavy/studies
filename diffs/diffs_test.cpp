359b0ff7a38b3dda532b14a74074818cca942cf4
Peter Lesslie <pclesslie@gmail.com>
2020-04-30 19:18:35 -0500

    More tests

--- diffs/diffs_test.cpp
+++ diffs/diffs_test.cpp
@@ -193,10 +193,13 @@ Quotes apply_diffs(const Quotes& current, const Diffs& diffs) {
     return result;
 }
 
-Diffs diff(const Quotes& current, Quotes& desired)
+Diffs diff(const Quotes& current, Quotes desired)
 {
     // std::sort(std::begin(current), std::end(current), QuotePriceCmp{});
     std::sort(std::begin(desired), std::end(desired), QuotePriceCmp{});
+
+    // TODO: add stub quote at the end of the array that is always invalid so
+    //       don't have to check size
 
     REQUIRE(std::is_sorted(std::begin(current), std::end(current), QuotePriceCmp{}));
     REQUIRE(std::is_sorted(std::begin(desired), std::end(desired), QuotePriceCmp{}));
@@ -231,7 +234,7 @@ Diffs diff(const Quotes& current, Quotes& desired)
             continue;
         }
         else if (QuotePriceCmp{}(current[idx1], desired[idx2])) { // current.px < desired.px
-            // TODO: slightly suboptimal, potentially could modify into next quote
+            // TODO: could peek 1 ahead and if it would be a ADD, then combine?
             Diff diff;
             diff.action = Action::eCancel;
             diff.px     = current[idx1].px;
@@ -241,6 +244,7 @@ Diffs diff(const Quotes& current, Quotes& desired)
             ++idx1;
             continue;
         } else {
+            // TODO: could peek 1 ahead and if it would be a DEL, then combine?
             assert(!QuotePriceCmp{}(current[idx1], desired[idx2])); // current.px >= desired.px
             Diff diff;
             diff.action = Action::eAdd;
@@ -355,6 +359,7 @@ TEST_CASE("Delete multiple prices")
     Quotes current = { Quote{1000, 1}, Quote{1002, 1}, Quote{1004, 2}, Quote{1005, 27}, Quote{1007, 42} };
 
     std::vector<Quotes> cases = {
+// orig { Quote{1000, 1}, Quote{1002, 1}, Quote{1004, 2}, Quote{1005, 27}, Quote{1007, 42} };
         {                 Quote{1002, 1}, Quote{1004, 2}, Quote{1005, 27}, Quote{1007, 42} },
         { Quote{1000, 1},                 Quote{1004, 2}, Quote{1005, 27}, Quote{1007, 42} },
         { Quote{1000, 1}, Quote{1002, 1},                 Quote{1005, 27}, Quote{1007, 42} },
@@ -373,5 +378,61 @@ TEST_CASE("Delete multiple prices")
         auto result = diff(current, desire);
         auto after  = apply_diffs(current, result);
         CHECK(after == desired);
+    }
+}
+
+Quote Q(uint32_t px) { return Quote{px, 1}; }
+Diff  ADD(uint32_t px, uint32_t qty) { return Diff{Action::eAdd, px, qty};    }
+Diff  DEL(uint32_t px, uint32_t qty) { return Diff{Action::eCancel, px, qty}; }
+Diff  CHG(uint32_t px, uint32_t orig_qty, uint32_t new_qty) { return Diff{Action::eCancel, px, new_qty}; }
+
+TEST_CASE("Move price")
+{
+    const Quotes current = { Q(1000), Q(1003), Q(1007), Q(1010), Q(1020) };
+
+    std::vector<Quotes> cases = {
+// orig { Q(1000), Q(1003), Q(1007), Q(1010), Q(1020) },
+        { Q(1000), Q(1003), Q(1007), Q(1010), Q(1021) },
+        { Q(1000), Q(1005), Q(1007), Q(1010), Q(1021) },
+        { Q(1000), Q(1005), Q(1009), Q(1010), Q(1021) },
+        { Q( 999), Q(1003), Q(1007), Q(1010), Q(1020) },
+        { Q( 999), Q(1003), Q(1007), Q(1010), Q(1029) },
+        { Q(1003), Q(1007), Q(1010), Q(1020), Q(1023) }, // cancel most aggressive, and add new least aggressive
+    };
+
+    for (const auto& desired : cases) {
+        INFO("BEFORE : " << current);
+        INFO("DESIRED: " << desired);
+        auto desire = desired;
+        auto result = diff(current, desire);
+        auto after  = apply_diffs(current, result);
+        CHECK(after == desired);
+        INFO("AFTER  : " << after);
+    }
+}
+
+TEST_CASE("Minimal diff for canceling inside price")
+{
+
+    SECTION("Cancel inside price add new outer price")
+    {
+        const Quotes current = { Q(1000), Q(1003), Q(1007), Q(1010), Q(1020) };
+        const Quotes desired = { Q(1003), Q(1007), Q(1010), Q(1020), Q(1023) };
+        const Diffs  expect = { DEL(1000, 1), ADD(1023, 1) };
+        auto         result = diff(current, desired);
+        auto         after  = apply_diffs(current, result);
+        CHECK(result == expect);
+        CHECK(after  == desired);
+    }
+
+    SECTION("Move inside price")
+    {
+        const Quotes current = { Q(1000), Q(1003), Q(1007), Q(1010), Q(1020) };
+        const Quotes desired = { Q(1000), Q(1004), Q(1008), Q(1010), Q(1020) };
+        const Diffs  expect = { DEL(1003, 1), ADD(1004, 1), DEL(1007, 1), ADD(1008, 1) };
+        auto         result = diff(current, desired);
+        auto         after  = apply_diffs(current, result);
+        CHECK(result == expect);
+        CHECK(after  == desired);
     }
 }
