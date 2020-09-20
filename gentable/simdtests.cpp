bf35d73590d3dd40a9d3319dd7d88df95adea49c
Peter Lesslie <pclesslie@gmail.com>
2020-09-20 14:04:52 -0500

    Working with 256

--- gentable/simdtests.cpp
+++ gentable/simdtests.cpp
@@ -4,8 +4,10 @@
 #include <immintrin.h>
 #include <fmt/ostream.h>
 
-constexpr size_t ALIGN = 32;
-constexpr size_t BITS_PER_OP  = 128;
+// #define BITS_PER_OP 128
+#define BITS_PER_OP 256
+
+constexpr size_t BITS_PER_OP_  = BITS_PER_OP;
 constexpr size_t BITS_PER_KEY = 32;
 constexpr size_t STRIDE = BITS_PER_OP / BITS_PER_KEY;
 constexpr size_t M = 4;
@@ -15,6 +17,14 @@ constexpr uint32_t vals[M] = {
     0xDEADBEEFu,
     0x0F0F0F0Fu,
 };
+
+#if   BITS_PER_OP == 128
+constexpr size_t ALIGN = 16;
+#elif BITS_PER_OP == 256
+constexpr size_t ALIGN = 32;
+#else
+#error "Invalid BITS_PER_OP"
+#endif
 
 void dump(const char* name, const uint32_t* vals)
 {
@@ -29,12 +39,6 @@ int main(int argc, const char** argv)
     size_t i, j;
     alignas(ALIGN) uint32_t keys[STRIDE];
     alignas(ALIGN) uint32_t outs[STRIDE];
-    __m128i  needle;
-    __m128i  haystk;
-    __m128i  result;
-    __m128   psresult;
-    __m128i  ones;
-    uint32_t mvmask;
 
     fmt::print("align        = {}\n", ALIGN);
     fmt::print("bits_per_op  = {}\n", BITS_PER_OP);
@@ -47,24 +51,40 @@ int main(int argc, const char** argv)
     dump("keys", keys);
 
     for (j = 0; j != M; ++j) {
+#if   BITS_PER_OP == 128
+        __m128i  needle;
+        __m128i  haystk;
+        __m128i  result;
+        __m128i  ones;
+        uint32_t mvmask;
+
         ones   = _mm_set1_epi32(0xFFFFFFFFu);
         needle = _mm_set1_epi32(vals[j]);
         haystk = _mm_load_si128((__m128i const*)&keys[0]);
         result = _mm_cmpeq_epi32(needle, haystk);
         result = _mm_xor_si128(result, ones);
+        _mm_store_si128((__m128i*)&outs[0], result);
+        mvmask = _mm_movemask_ps((__m128)result);
+#elif BITS_PER_OP == 256
+        __m256i needle;
+        __m256i haystk;
+        __m256i result;
+        __m256i ones;
+        uint32_t mvmask;
 
-#ifdef DOES_NOT_WORK
-        needle = _mm_set1_epi32(vals[3]);
-        haystk = _mm_load_si128((__m128i const*)&keys[0]);
-        psresult = _mm_cmpneq_ps((__m128)needle, (__m128)haystk);
-        _mm_store_ps((float*)&outs[0], psresult);
-        mvmask = _mm_movemask_ps(psresult);
+        ones   = _mm256_set1_epi32(0xFFFFFFFFu);
+        needle = _mm256_set1_epi32(vals[j]);
+        haystk = _mm256_load_si256((__m256i const*)&keys[0]);
+        result = _mm256_cmpeq_epi32(needle, haystk);
+        result = _mm256_xor_si256(result, ones);
+        _mm256_store_si256((__m256i*)&outs[0], result);
+        mvmask = _mm256_movemask_ps((__m256)result);
+#else
+#error "Invalid BITS_PER_OP"
 #endif
 
-        _mm_store_si128((__m128i*)&outs[0], result);
         fmt::print("j = {}\n", j);
         dump("outs", outs);
-        mvmask = _mm_movemask_ps((__m128)result);
         for (size_t i = 0; i != STRIDE; ++i) {
             fmt::print("mvmask[{:d}] = {:d}\n", i, (mvmask >> i) & 0x1u);
         }
