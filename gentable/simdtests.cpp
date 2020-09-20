a5f8ed71f8798752da4fb3bf8015bd6a48d7bc1f
Peter Lesslie <pclesslie@gmail.com>
2020-09-20 13:45:37 -0500

    Verify doesn't work with floats

--- gentable/simdtests.cpp
+++ gentable/simdtests.cpp
@@ -16,21 +16,24 @@ constexpr uint32_t vals[M] = {
     0x0F0F0F0Fu,
 };
 
-void dump_keys(const uint32_t* keys)
+void dump(const char* name, const uint32_t* vals)
 {
     for (size_t i = 0; i != STRIDE; ++i) {
-        fmt::print("keys[{:d}] = 0x{:08x};\n", i, keys[i]);
+        fmt::print("{:s}[{:d}] = 0x{:08x};\n", name, i, vals[i]);
     }
+    fmt::print("\n");
 }
 
 int main(int argc, const char** argv)
 {
-    size_t i;
+    size_t i, j;
     alignas(ALIGN) uint32_t keys[STRIDE];
     alignas(ALIGN) uint32_t outs[STRIDE];
     __m128i  needle;
     __m128i  haystk;
     __m128i  result;
+    __m128   psresult;
+    __m128i  ones;
     uint32_t mvmask;
 
     fmt::print("align        = {}\n", ALIGN);
@@ -41,18 +44,33 @@ int main(int argc, const char** argv)
     for (i = 0; i != STRIDE; ++i) {
         keys[i] = vals[i % M];
     }
-    dump_keys(keys);
+    dump("keys", keys);
 
-    needle = _mm_set1_epi32(vals[3]);
-    haystk = _mm_load_si128((__m128i const*)&keys[0]);
-    result = _mm_cmpeq_epi32(needle, haystk);
-    // TODO: try _mm_cmpneq_ps(__m128, __m128)
-    _mm_store_si128((__m128i*)&outs[0], result);
-    dump_keys(outs);
-    mvmask = _mm_movemask_ps((__m128)result);
-    for (size_t i = 0; i != STRIDE; ++i) {
-        fmt::print("mvmask[{:d}] = {:d}\n", i, (mvmask >> i) & 0x1u);
+    for (j = 0; j != M; ++j) {
+        ones   = _mm_set1_epi32(0xFFFFFFFFu);
+        needle = _mm_set1_epi32(vals[j]);
+        haystk = _mm_load_si128((__m128i const*)&keys[0]);
+        result = _mm_cmpeq_epi32(needle, haystk);
+        result = _mm_xor_si128(result, ones);
+
+#ifdef DOES_NOT_WORK
+        needle = _mm_set1_epi32(vals[3]);
+        haystk = _mm_load_si128((__m128i const*)&keys[0]);
+        psresult = _mm_cmpneq_ps((__m128)needle, (__m128)haystk);
+        _mm_store_ps((float*)&outs[0], psresult);
+        mvmask = _mm_movemask_ps(psresult);
+#endif
+
+        _mm_store_si128((__m128i*)&outs[0], result);
+        fmt::print("j = {}\n", j);
+        dump("outs", outs);
+        mvmask = _mm_movemask_ps((__m128)result);
+        for (size_t i = 0; i != STRIDE; ++i) {
+            fmt::print("mvmask[{:d}] = {:d}\n", i, (mvmask >> i) & 0x1u);
+        }
     }
+
+
 
     // __m128i result;
     // int     mask;
