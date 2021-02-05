672e6c3309c65e6153492d7806e491192ad92374
Peter Lesslie <pclesslie@gmail.com>
2021-02-05 14:47:22 -0600

    A few more tests

--- cstr/cstr.test.cxx
+++ cstr/cstr.test.cxx
@@ -2,22 +2,28 @@
 #include <cstr.h>
 #include <string>
 #include <cstring>
+#include <vector>
 
 
 TEST_CASE("string length")
 {
-    std::string expect = "Hello, World";
-    cstr* s = cstr_new(expect.c_str(), expect.size());
+    std::vector<std::string> cases = {
+        "",
+        "Hello, World",
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaa;laskjdfl;kjasl;dkfjaskl;jdfkl;jasdf",
+        "  asdf Ha;lskdfn",
+        "\001FIX4.2\00142=Value\001",
+    };
 
-    REQUIRE(s != NULL);
-    CHECK(cstr_size(s)    == expect.size());
-    CHECK(cstr_len(s)     == expect.size());
-    CHECK(cstr_length(s)  == expect.size());
-
-    CHECK(cstr_capacity(s) >= expect.size());
-
-    CHECK(strlen(cstr_str(s)) == expect.size());
-    CHECK(strcmp(cstr_str(s), expect.c_str()) == 0);
-
-    cstr_del(s);
+    for (const auto expect : cases) {
+        cstr* s = cstr_new(expect.c_str(), expect.size());
+        REQUIRE(s != NULL);
+        CHECK(cstr_size(s)    == expect.size());
+        CHECK(cstr_len(s)     == expect.size());
+        CHECK(cstr_length(s)  == expect.size());
+        CHECK(cstr_capacity(s) >= expect.size());
+        CHECK(strlen(cstr_str(s)) == expect.size());
+        CHECK(strcmp(cstr_str(s), expect.c_str()) == 0);
+        cstr_del(s);
+    }
 }
