6332375032bbc722895516014a4f41f516ead348
Peter Lesslie <pclesslie@gmail.com>
2021-02-15 15:42:04 -0600

    use new super library

--- abi-compat/test_super.cxx
+++ abi-compat/test_super.cxx
@@ -1,20 +1,18 @@
 #include <iostream>
-#include "super.h"
+#include <super/super.h>
 
 int main(int argc, const char** argv)
 {
     (void)argc;
     (void)argv;
 
-    auto* super = super_new();
-    if (!super) {
-        super_del(super);
-        std::cerr << "error: failed to create super object" << std::endl;
-        return 0;
+    auto s = super::make(1, 2);
+
+    if (s.x == 0) {
+        std::cout << "s.x == 0" << std::endl;
+    } else {
+        std::cout << "s.x != 0" << std::endl;
     }
-    auto result = super_calc(super, 42);
-    std::cout << "result: " << result << "\n";
-    super_print(super);
-    super_del(super);
+
     return 0;
 }
