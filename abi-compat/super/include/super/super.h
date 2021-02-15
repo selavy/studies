519e718f6745966fd1f280faee1d98b305b996da
Peter Lesslie <pclesslie@gmail.com>
2021-02-15 15:38:41 -0600

    Try again setting up mock library

---
+++ abi-compat/super/include/super/super.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace super {
+
+struct Pair
+{
+    int x;
+    int y;
+};
+
+extern Pair make(int x, int y);
+
+}  // namespace super
---
+++ abi-compat/super/src/super.cxx
@@ -0,0 +1,10 @@
+#include <super/super.h>
+
+namespace super {
+
+Pair make(int x, int y)
+{
+    return { x, y };
+}
+
+}  // namespace super
